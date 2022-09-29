863f1fbbe19fba2c726bb9f790500e37d68b8717
Joan RACENET <joan.racenet@smartesting.com>
2022-09-29 16:06:47 +0200

    Keep working on some UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,17 @@ const Home: NextPage = () => {
   }
 
   return (
-    <Layout meta={meta}></Layout>
+    <Layout meta={meta}>
+      <h2>What is Gravity?</h2>
+      <p>
+        Gravity helps Agile & DevOps teams to deliver high-quality software faster, by enabling them select and design end-to-end tests accordingly to HOW their users truly interact with their software.
+
+        We call it a Usage-Driven Testing platform.
+      </p>
+
+      <video src="https://www.gravity-testing.com/wp-content/uploads/2022/09/Gravity-Presentation-short-format-.mp4"  controls={false} controlsList="nodownload"></video>
+
+    </Layout>
   )
 }
 

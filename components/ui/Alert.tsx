f611926eaf592a0d13578e94f55f35f4df9ffe64
Joan RACENET <joan.racenet@smartesting.com>
2022-10-06 15:27:57 +0200

    Add Alert componenet and use it

---
+++ components/ui/Alert.tsx
@@ -0,0 +1,33 @@
+import styles from './Alert.module.scss';
+
+export enum AlertType {
+  "info",
+  "success",
+  "warning",
+  "danger",
+}
+
+export default function Alert({title, type, children}: React.PropsWithChildren<{title?: string, type: AlertType}>) {
+
+  let computedClass = "";
+
+  switch(type) {
+    case AlertType.info:
+      computedClass = styles.alertInfo;
+      break;
+    case AlertType.success:
+      computedClass = styles.alertSuccess;
+      break;
+    case AlertType.warning:
+      computedClass = styles.alertWarning;
+      break;
+    case AlertType.danger:
+      computedClass = styles.alertDanger;
+      break;
+  }
+
+  return <div className={`${styles.alert} ${computedClass}`}>
+    {title? <h5>{title}</h5> : <></>}
+    { children }
+  </div>
+}
